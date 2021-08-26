e826a131a4f6631e52b36db8dad1d931d03d0f8d
Garance Clausse <garanceclausse@msn.com>
2021-08-26 17:59:35 +0200

    csp modified + authorization token

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const contentSecurityPolicy = require("helmet-csp");
-
+var helmet = require('helmet-csp');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
@@ -13,20 +12,7 @@ require('dotenv').config({ path: process.cwd() + '/.env' });
 
 const app = express();
 
-app.use(
-  contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      defaultSrc: ["'self'", "img-src *", "images"],
-      scriptSrc: ["'self'", "js.example.com"],
-      objectSrc: ["'none'"],
-      upgradeInsecureRequests: [],
-    },
-    reportOnly: false,
-  })
-);
-
-
+app.use(helmet());
 /***Connecte notre serveur à mongodb / la base de donnée */
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
@@ -42,17 +28,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    /*res.setHeader('Content-Security-Policy',  "default-src img-src style-src  * 'self' images");*/
     next();
 });
-
-
 app.use(bodyParser.json());
-
 app.use(express.static('../frontend'));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
-
 module.exports = app;
